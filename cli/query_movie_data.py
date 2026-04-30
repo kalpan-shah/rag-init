bc465a78b08d53f31b7c39118f37f1bdc284ed25
kalpan <kalpanshah106@gmail.com>
2026-04-30 09:50:54 +0000

    feat: Added punctuation removal
     - Seperated the function for text processing, two steps added so far

--- cli/query_movie_data.py
+++ cli/query_movie_data.py
@@ -9,7 +9,8 @@
 from typing import List
 import os
 import json
- 
+import string
+
 # Assuming running from the project root, adjust path accordingly later if needed
 movie_data_json = os.path.join(os.getcwd(),'data', 'movies.json')
 
@@ -23,15 +24,35 @@ def load_data(file_path: str) -> dict:
 
 movie_data: List[dict] = load_data(movie_data_json)['movies']
 
+def preprocess_text(text: str) -> str:
+    """
+        Helper function to preprocess text for better search results
+        a. Case insensitive search
+        b. punctuation removal
+    """
+    # a. Case insensitive search
+    text = text.lower() 
+
+    # b. punctuation removal 
+    # Define a translation table that maps punctuation characters to None
+    tab = str.maketrans('', '', string.punctuation)  # mapping for removing punctuation
+    text = text.translate(tab)  # mapping applied using translate method
+
+    return text
+
 def search_movies(query: str) -> List[dict]:
     """
         V1 - Search for movies if the movie name contains the query string 
+            Text Processing:
+                a. Case insensitive search
+                b. punctuation removal
     """
     results = []
     for movie in movie_data:
         if not isinstance(movie, dict) and 'title' not in movie:
             continue
-        
-        if query.lower() in movie['title'].lower():
+        _query = preprocess_text(query)
+        _movie_title = preprocess_text(movie['title'])
+        if _query in _movie_title:
             results.append(movie)
     return results
